df8647678fa283cf9e9f13cd81998c896ef84f45
Lovro Hrust <hrustlovro@gmail.com>
2024-08-02 02:53:07 +0200

    better explanations

--- editor-tweak.js
+++ editor-tweak.js
@@ -2,8 +2,13 @@
  * Change editor defaults
  */
 
+// all is wrapped in async function, because await is used to get suspendSelect value
+// if plain select were used, we would again have to have some kind of loop to wait for its results
+// because it returns undefined untill value is fetched from REST
+
 (async () => {
-	// use new suspendSelect with awkward syntax
+	// use new suspendSelect with awkward syntax - it seems I am the first using it,
+	// so I had to figure it out how to use it
 	async function getSuspended(callback) {
 		let value;
 		try {
@@ -31,7 +36,10 @@
 		key,
 		userIsAdmin
 	)};
-	//  set block list to open in left sidebar, block breadcrumbs, right-click menus in list view and most used blocks in menu to show for admin. For others, not.
+
+	// set block list to open in left sidebar, block breadcrumbs, right-click menus in list view and most used blocks in menu to show for admin. For others, not.
+	// you can find other keys by installing Redux browser extension, changing preference in UI and looking at DIFF tab in core/preferences to see what has changed
+	// remember to select on left side the last store getter
 	keys = ['showListViewByDefault', 'showBlockBreadcrumbs', 'allowRightClickOverrides', 'mostUsedBlocks'];
 	keys.forEach((key) => {
 		setter(key);
