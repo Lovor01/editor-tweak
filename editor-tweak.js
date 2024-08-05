9fa996bded526fe2ca7e75cb6b838a54228b66e5
Lovro Hrust <hrustlovro@gmail.com>
2024-08-05 13:42:57 +0200

    fixed bugs

--- editor-tweak.js
+++ editor-tweak.js
@@ -1,21 +1,26 @@
 /**
  * Change editor defaults
  */
+/* eslint-disable no-undef */
 
 // all is wrapped in async function, because await is used to get suspendSelect value
 // if plain select were used, we would again have to have some kind of loop to wait for its results
 // because it returns undefined untill value is fetched from REST
 
 /**
- * set block editor preferences according to username
+ * set block editor preferences according to username or role
  *
  * @param {Object|Object[]} preferenceKeys your preference keys, string for single or array for multiple
  * @param {String} preferenceKeys.namespace - preference namespace
  * @param {String} preferenceKeys.key - preference key
  * @param {String} preferenceKeys.value - preference value to set
- * @param {String} username - user's username
+ * @param {String} userIdentifier - user's username
+ * @param {String} mode - mode of operation, can be 'role' or 'username'
+ * @return {Promise} - void promise
+ * @example
+ * setUsernamePreferences({namespace: 'core', key: 'showIconLabels', value: true}, 'admin', 'username');
  */
-const setUsernamePreferences = async ( preferenceKeys, username ) => {
+const conditionalSetPreferences = async ( preferenceKeys, userIdentifier, mode ) => {
 	// use new suspendSelect with awkward syntax - it seems I am the first using it,
 	// so I had to figure it out how to use it
 	async function getSuspended(callback) {
@@ -29,13 +34,38 @@ const setUsernamePreferences = async ( preferenceKeys, username ) => {
 		return value
 	}
 
+	const throwParameterError = () => {
+		throw new Error(
+			`Function conditionalSetPreferences: userIndentifier (second parameter) was specified, but third parameter to describe second parameter
+			which can be 'username' or 'role' is not specified correctly!`);
+	};
+
 	const suspendedSelector = wp.data.suspendSelect('core');
 	// userShort because currentUser does not return all properties of user
 	const userID = (await getSuspended(suspendedSelector.getCurrentUser)).id;
 	const user = await getSuspended(() => suspendedSelector.getUser(userID));
-	// test if username matches, if not bail out
-	if (username !== undefined && user.username !== username) {
-		return;
+
+	if (userIdentifier !== undefined && mode === undefined) {
+		throwParameterError();
+	}
+	switch (mode) {
+		case 'role':
+		{
+			const userRoles = user.roles;
+			if (! userRoles.includes(userIdentifier)) {
+				return;
+			}
+			break;
+		}
+		case 'username':
+			// test if username matches, if not bail out
+			if (userIdentifier !== undefined && user.username !== userIdentifier) {
+				return;
+			};
+			break;
+		default:
+			if (mode !== undefined)
+				throwParameterError();
 	}
 
 	const setter = (namespaceKeyValue) => {wp.data.dispatch('core/preferences').set(
@@ -60,14 +90,16 @@ const setUsernamePreferences = async ( preferenceKeys, username ) => {
  * This is where you put your own code,
  * call setUsernamePreferences to set preferences according to user
  */
-// set icon labels to true, just for username admin
-setUsernamePreferences({namespace: 'core', key: 'showIconLabels', value: true}, 'admin');
+// set icon labels to false for administrator role, true for editor
+conditionalSetPreferences({namespace: 'core', key: 'showIconLabels', value: false}, 'administrator', 'role');
+conditionalSetPreferences({namespace: 'core', key: 'showIconLabels', value: true}, 'editor', 'role');
+// set theme styles to true for admin username
+conditionalSetPreferences({namespace: 'core/edit-post', key: 'themeStyles', value: true}, 'admin', 'username');
 // set preferences for all users
-setUsernamePreferences([
+conditionalSetPreferences([
 	// Contain text cursor inside block
 	{namespace: 'core', key: 'keepCaretInsideBlock', value: true},
 	// Distraction free
 	{namespace: 'core', key: 'focusMode', value: false},
 	// Use theme styles
-	{namespace: 'core/edit-post', key: 'themeStyles', value: true},
 ]);
